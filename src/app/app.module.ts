503427c915897d591b5a32aad485e6c771f76fb7
s3rl3g <serleg.dani@live.com>
2018-06-10 11:04:10 +0200

    radar component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RadarComponent } from './radar/radar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RadarComponent
   ],
   imports: [
     BrowserModule
